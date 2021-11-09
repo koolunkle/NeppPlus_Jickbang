6d20d6ca1c4b91b629a60e20dc4f030a215b2b29
김준기 <gugi329@daum.net>
2021-11-09 23:51:11 +0900

    실제 상세 정보 표시  - 클래스에 만든 함수들 재활용

--- app/src/main/java/com/neppplus/jickbangcopy_20211104/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211104/ViewRoomDetailActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.jickbangcopy_20211104
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.jickbangcopy_20211104.datas.RoomData
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,12 @@ class ViewRoomDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_room_detail)
 
         val roomData = intent.getSerializableExtra("room") as RoomData
+
+        txtPrice.text = roomData.getFormattedPrice()
+        txtDescription.text = roomData.description
+
+        txtAddress.text = roomData.address
+        txtFloor.text = roomData.getFormattedFloor()
 
     }
 }
