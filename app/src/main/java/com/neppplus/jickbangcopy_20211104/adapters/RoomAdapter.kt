358a5b98164e59ec2f20e6f435c9752b963a3d68
김준기 <gugi329@daum.net>
2021-11-09 23:26:29 +0900

    RoomAdapter 추가 + ArrayAdapter 상속  - 기본 생성자 지원 X, 추가 작업 필요

---
+++ app/src/main/java/com/neppplus/jickbangcopy_20211104/adapters/RoomAdapter.kt
@@ -0,0 +1,7 @@
+package com.neppplus.jickbangcopy_20211104.adapters
+
+import android.widget.ArrayAdapter
+import com.neppplus.jickbangcopy_20211104.datas.RoomData
+
+class RoomAdapter : ArrayAdapter<RoomData>() {
+}
