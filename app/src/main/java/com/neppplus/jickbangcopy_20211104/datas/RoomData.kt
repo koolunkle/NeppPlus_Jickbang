6beea76ca82fb806607283dd762fe2d58c049896
김준기 <gugi329@daum.net>
2021-11-09 23:39:11 +0900

    1억 이상인 경우에 대한 대응   - 억단위 구하기 / 나머지 구하기   - 두 값을 합친 String으로 결과 선정

--- app/src/main/java/com/neppplus/jickbangcopy_20211104/datas/RoomData.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211104/datas/RoomData.kt
@@ -1,6 +1,8 @@
 package com.neppplus.jickbangcopy_20211104.datas
 
 import android.icu.text.NumberFormat
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.util.*
 
 class RoomData(
@@ -11,6 +13,7 @@ class RoomData(
 
     //    가격에 따라 다른 형태로 가공해서 가격을 알려주는 함수
 
+    @RequiresApi(Build.VERSION_CODES.N)
     fun getFormattedPrice() : String {
 
         if (this.price < 10000) {
@@ -20,7 +23,13 @@ class RoomData(
 
         }
         else {
-            val priceStr = NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)
+
+            val uk = this.price / 10000
+            val rest = this.price % 10000
+            val restCommaStr = NumberFormat.getNumberInstance(Locale.KOREA).format(rest)
+
+            val priceStr = "${uk}억 ${restCommaStr}"
+
             return priceStr
         }
 
