3cb05abeb347c42a892c2ce655ecd7f087762616
김준기 <gugi329@daum.net>
2021-11-09 23:48:00 +0900

    방 상세 화면 UI 작업

---
+++ app/src/main/java/com/neppplus/jickbangcopy_20211104/ViewRoomDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.jickbangcopy_20211104
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail)
+    }
+}
